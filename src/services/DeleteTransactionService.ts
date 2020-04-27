5e76a6ec884b4a28b4f737d4af91481433787e14
denismend <denisladeira1@gmail.com>
2020-04-26 21:42:17 -0300

    implement post, get and delete transaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,23 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+import TransactionRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getRepository(TransactionRepository);
+
+    const transaction = await transactionRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist !');
+    }
+
+    await transactionRepository.remove(transaction);
   }
 }
 
