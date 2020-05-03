319ccf5a336422f2bf02cd7b932bdd79c0e60423
denismend <denisladeira1@gmail.com>
2020-05-03 17:17:20 -0300

    use cors and return category data in transaction

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -4,7 +4,9 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
 } from 'typeorm';
+import Transaction from './Transaction';
 
 @Entity('categories')
 class Category {
@@ -20,6 +22,9 @@ class Category {
   @Column({ select: false })
   @UpdateDateColumn({ select: false })
   updated_at: Date;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transaction: Transaction;
 }
 
 export default Category;
